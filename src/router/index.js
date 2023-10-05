72e7fb99f5205738229994e17574b934308166c5
bautistaamarillo <amarillobautista5@gmail.com>
2023-10-05 18:55:37 -0300

    agrego una nueva vista

--- src/router/index.js
+++ src/router/index.js
@@ -14,6 +14,12 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+  {
+    path: '/testeo',
+    name: 'testeo',
+    
+    component: () => import(/* webpackChunkName: "testeo" */ '../views/TestView.vue')
   }
 ]
 
---
+++ src/views/TestView.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+      <h1>Prueba, pasaje de datos.</h1>
+      <IngresarDatos />
+    </div>
+  </template>
+  
+  <script>
+ 
+  
+  export default {
+    components: {
+      
+    },
+  };
+  </script>
