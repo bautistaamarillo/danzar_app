051998bd97b1578ace3ee083f6f76b6ca6214c2f
gianellaromero <romerogianellaa@gmail.com>
2023-10-05 19:30:26 -0300

    probando vista testeo

--- src/views/TestView.vue
+++ src/views/TestView.vue
@@ -1,16 +1,31 @@
 <template>
-    <div>
-      <h1>Prueba, pasaje de datos.</h1>
-      <IngresarDatos />
+    <div id="app">
+      <div class="ui fixed inverted menu vue-color">
+        <div class="ui container">
+          <a href="#" class="header item"> Hola </a>
+        </div>
+      </div>
+      <div class="ui main container">
+
+       <Students />
+
+
+      </div>
     </div>
   </template>
   
   <script>
- 
-  
+
+ import Students from '@/components/Students.vue';
   export default {
+    name:"App",
     components: {
-      
-    },
+      Students
+    }
   };
   </script>
+  <style>
+.vue-color{
+  background:rgb(0, 162, 255) !important ;
+}
+</style>
