af2a9fd7f72dae4628219ab17650938cff0b9be1
bautistaamarillo <amarillobautista5@gmail.com>
2023-10-06 15:16:49 -0300

    add creo un nuevo componente y vista dashboard

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,18 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
+
 const routes = [
   {
     path: '/',
     name: 'home',
     component: HomeView
+  },
+  {
+    path: '/dashboard',
+    name: 'dashboard',
+    
+    component: () => import(/* webpackChunkName: "dashboard" */ '../views/DashboardView.vue')
   },
   {
     path: '/about',
