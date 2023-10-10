ddd37966a09b7b447a4b2cc0f654696056ea8192
bautistaamarillo <amarillobautista5@gmail.com>
2023-10-10 19:50:53 -0300

    datos extrapolados, funcion getDate

---
+++ src/components/FuncionesUtilidades.vue
@@ -0,0 +1,29 @@
+<template>
+
+</template>
+
+<script>
+
+import axios from "axios";
+export default {
+  name: 'FuncionesUtilidades',
+  methods: {
+    async getData(endpoint) {
+    axios
+      .get("http://localhost/danzar_api/public/"+endpoint)
+      .then((response) => {
+        // Mostramos los datos obtenidos en la consola
+        console.log(response);
+        this.students = response.data;
+      })
+      .catch(function (error) {
+        // Si hubo algun error mostramos algo
+        console.log(error);
+      });
+    },
+  },
+  
+};
+
+
+</script>
--- src/views/TestView.vue
+++ src/views/TestView.vue
@@ -1,56 +1,45 @@
 <template>
-    <div id="app">
-      <!-- <div class="ui fixed inverted menu vue-color"> -->
-        <div class="ui container">
-          <a href="#" class="header item"> Lista Estudiantes </a>
-        </div>
-      </div>
-      <div class="ui main container">
-
-       <StudentList />
-
-
-      </div>
-    <!-- </div> -->
-    <tr v-for="student in students" :key="student.id">
-            <td>{{ student.id }}</td>
-            <td>{{ student.name }}</td>
-          </tr>
-  </template>
+  <div id="app">
+    <!-- <div class="ui fixed inverted menu vue-color"> -->
+    <div class="ui container">
+      <a href="#" class="header item"> Lista Estudiantes </a>
+    </div>
+  </div>
+  <div class="ui main container">
+    <StudentList />
+  </div>
+  <!-- </div> -->
+  <tr v-for="student in students" :key="student.id">
+    <td>{{ student.id }}</td>
+    <td>{{ student.name }}</td>
+  </tr>
+</template>
   
   <script>
+import StudentList from "@/components/StudentList.vue";
+import FuncionesUtilidades from "@/components/FuncionesUtilidades.vue";
 
- import StudentList from '@/components/StudentList.vue';
- import axios from "axios";
-  export default {
-    name:"App",
-     data() {
+//  import axios from "axios";
+export default {
+  name: "App",
+  data() {
     return {
       students: [],
     };
   },
   components: {
-      StudentList
-    },
+    StudentList,
+  },
+  mixins: [FuncionesUtilidades],
+
   mounted() {
-    axios
-      .get("http://localhost/danzar_api/public/students")
-      .then((response) => {
-        // Mostramos los datos obtenidos en la consola
-        console.log(response);
-        this.students = response.data;
-      })
-      .catch(function (error) {
-        // Si hubo algun error mostramos algo
-        console.log(error);
-      });
+    this.getData("students");
   },
-  methods: {
-  },
-  };
-  </script>
+  methods: {},
+};
+</script>
   <style>
-.vue-color{
-  background:rgb(0, 162, 255) !important ;
+.vue-color {
+  background: rgb(0, 162, 255) !important ;
 }
 </style>
