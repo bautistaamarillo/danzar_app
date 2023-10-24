f5ca2935576d9c7a77e44f10321e8b8db1fbc1b2
bautistaamarillo <amarillobautista5@gmail.com>
2023-10-24 20:07:02 -0300

    categoria abm con modal

--- src/views/AbmCategoryView.vue
+++ src/views/AbmCategoryView.vue
@@ -1,58 +1,53 @@
 <template>
-    <div>
-      <h1>Operar Categorias</h1>
-      <form>
-        
-        Nombre
-        <input type="text" v-model="registro.name"> <br>
-        <input type="submit" value="nuevo" @click="crear()">
-      </form>
-    </div>
-  </template>
-  
+  <div>
+    <h1>Operar Categorias</h1>
+    <form>
+      Nombre
+      <input type="text" v-model="registro.name" /> <br />
+      <input type="submit" value="nuevo" @click="crear()" />
+    </form>
+  </div>
+</template>
+
   <script>
- 
- import axios from "axios";
- import FuncionesUtilidades from "@/components/FuncionesUtilidades.vue";
-  export default {
-    components: {
+import axios from "axios";
+import FuncionesUtilidades from "@/components/FuncionesUtilidades.vue";
+export default {
+  components: {},
+  data() {
+    return {
+      registro: {
+        name: "",
+      },
+    };
+  },
 
-    },
-    data() {
-        return {
-        registro: {
-          name: ""
-        },
-        };
-    },
+  mixins: [FuncionesUtilidades],
 
-    mixins: [FuncionesUtilidades],
+  mounted() {
+    this.getData("categories", 1);
+  },
 
-mounted() {
-  this.getData("categories",1);
-},
-
-    methods: {
-      crear() {
-        var url = "http://localhost/danzar_api/public/categories"
-        var msg = this.registro
-        console.log(url, msg)
-          axios
-          .post(url, msg)
-          .then((response) => {
+  methods: {
+    crear() {
+      var url = "http://localhost/danzar_api/public/categories";
+      var msg = this.registro;
+      console.log(url, msg);
+      axios
+        .post(url, msg)
+        .then((response) => {
           // Mostramos los datos obtenidos en la consola
           console.log(response);
           this.categorias = response.data;
-          })
-          .catch(function (error) {
+        })
+        .catch(function (error) {
           // Si hubo algun error mostramos algo
           console.log(error);
-          });
-      },
-      editar() {
-        axios
-        .put
-      }
-    }
-  };
-  </script>
+        });
+    },
+    editar() {
+      axios.put;
+    },
+  },
+};
+</script>
--- src/views/InfoCategoryView.vue
+++ src/views/InfoCategoryView.vue
@@ -1,104 +1,157 @@
 <template>
+  <div>
     <div class="card">
-  <div class="card-body">
-    <h1> Estas son las categorias. </h1>
-  </div>
-</div>
+      <div class="card-body">
+        <h1>Estas son las categorias.</h1>
+      </div>
+    </div>
 
-<table class="table table-striped">
-  <thead>
-    <tr>
-      <th scope="col">ID</th>
-      <th scope="col">Nombre</th>
-      <th scope="col">Acciones</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr v-for="categoria in categorias" :key="categoria.id">
-            <td>{{ categoria.id }}</td>
-            <td>{{ categoria.name }}</td>
-            <td>  <button type="button" class="btn btn-outline-warning" @click="editar()">Editar</button>
-              <button type="button" class="btn btn-outline-danger" @click="eliminar()">  Eliminar </button>
-              <button type="button" class="btn btn-outline-info" @click="consultar()">Info</button>
-              </td>
-          </tr>
+    <table class="table table-striped">
+      <thead>
+        <tr>
+          <th scope="col">ID</th>
+          <th scope="col">Nombre</th>
+          <th scope="col">Acciones</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="categoria in categorias" :key="categoria.id">
+          <td>{{ categoria.id }}</td>
+          <td>{{ categoria.name }}</td>
+          <td>
+            <button
+              type="button"
+              class="btn btn-outline-warning"
+              @click="editar()"
+            >
+              Editar
+            </button>
+            <button
+              type="button"
+              class="btn btn-outline-danger"
+              @click="eliminar()"
+            >
+              Eliminar
+            </button>
+            <button
+              type="button"
+              class="btn btn-outline-info"
+              @click="consultar()"
+            >
+              Info
+            </button>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+
+    <!-- Button trigger modal -->
+    <button
+      type="button"
+      class="btn btn-primary"
+      @click="categoriaAbmVisible = !categoriaAbmVisible "
+      data-bs-toggle="modal"
+      data-bs-target="#exampleModal"
+    >
+      Crear Nueva
+    </button>
+
+    <!-- Modal -->
+    <div
+      class="modal fade"
+      id="exampleModal"
+      tabindex="-1"
+      aria-labelledby="exampleModalLabel"
+      aria-hidden="true"
+    >
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">Nueva Categoria</h5>
+            <button
+              type="button"
+              class="btn-close"
+              data-bs-dismiss="modal"
+              aria-label="Close"
+            ></button>
+          </div>
+          <div class="modal-body"> 
+
+            <div v-if="categoriaAbmVisible === true">
+              <AbmCategoryView />
+            </div>
+
+          </div>
           
-  </tbody>
-</table>
+        </div>
+      </div>
+    </div>
 
-    Añadir Nuevo:<input v-model="check" type="checkbox"> 
-     <div v-if="check === true">
-    <AbmCategoryView/>
-     </div>
-      </template>
+    <!-- <button type="button"  @click="categoriaAbmVisible=!categoriaAbmVisible">Añadir Nuevo</button>  -->
+  </div>
+</template>
 
 
 <script>
-import AbmCategoryView from "./AbmCategoryView.vue"
+import AbmCategoryView from "./AbmCategoryView.vue";
 import axios from "axios";
 export default {
   data() {
-    
     return {
-      check: false,
+      categoriaAbmVisible: false,
       categorias: [],
     };
   },
   components: {
-    AbmCategoryView
-    
+    AbmCategoryView,
   },
   mounted() {
     axios
       .get("http://localhost/danzar_api/public/categories")
       .then((response) => {
-        
         console.log(response);
         this.categorias = response.data;
       })
       .catch(function (error) {
-      
         console.log(error);
       });
   },
   methods: {
-        modificar() {
-          var url = "http://localhost/danzar_api/public/categories/${categoria.id}"
-          var msg = {name: this.name}
-          console.log(url, msg)
-            axios
-            .put(url, msg)
-            .then((response) => {
-          
-            console.log(response);
-            this.categorias = response.data;
-            })
-            .catch(function (error) {
-            // Si hubo algun error mostramos algo
-            console.log(error);
-            });
-        },
-        eliminar() {
-          console.log("hola");
-          // var url = "http://localhost/danzar_api/public/categories/"
-          // var msg = {name: this.name}
-          // console.log(url, msg)
-          //   axios
-          //   .delete(url, msg)
-          //   .then((response) => {
-          //   // Mostramos los datos obtenidos en la consola
-          //   console.log(response);
-          //   this.categorias = response.data;
-          //   })
-          //   .catch(function (error) {
-          //   // Si hubo algun error mostramos algo
-          //   console.log(error);
-          //   });
-        }
+    modificar() {
+      var url = "http://localhost/danzar_api/public/categories/${categoria.id}";
+      var msg = { name: this.name };
+      console.log(url, msg);
+      axios
+        .put(url, msg)
+        .then((response) => {
+          console.log(response);
+          this.categorias = response.data;
+        })
+        .catch(function (error) {
+          // Si hubo algun error mostramos algo
+          console.log(error);
+        });
+    },
+    eliminar() {
+      console.log("hola");
+      // var url = "http://localhost/danzar_api/public/categories/"
+      // var msg = {name: this.name}
+      // console.log(url, msg)
+      //   axios
+      //   .delete(url, msg)
+      //   .then((response) => {
+      //   // Mostramos los datos obtenidos en la consola
+      //   console.log(response);
+      //   this.categorias = response.data;
+      //   })
+      //   .catch(function (error) {
+      //   // Si hubo algun error mostramos algo
+      //   console.log(error);
+      //   });
+    },
   },
 };
 </script>
 
 <style>
-
 </style>
